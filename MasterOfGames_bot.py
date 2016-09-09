8eaaed692efb73e36744598a9ac3aaf6be7539ae
Zachary R <zachary.mt.ranes@gmail.com>
2016-09-09 15:00:12 -0400

    Game still part of bot

--- MasterOfGames_bot.py
+++ MasterOfGames_bot.py
@@ -13,28 +13,37 @@ bot = telebot.TeleBot(config.get("telegram_bot_api","telegram_token"))
 
 bot.game_state = 0
 bot.players = []
+bot.spys= []
 bot.game_room = None
 bot.number_of_players = None
 bot.number_of_spys = None
+bot.round_number = None
+bot.points_resistance = 0
+bot.points_spys = 0
 
 
 
 @bot.message_handler(commands=['start'])
-def new_player(message):
+def NewPlayer(message):
 	if message.chat.type == "group":
 		bot.reply_to(message, "Hi I am a bot that lets you play games in a chat group \nRun the /new_game command to begin a new game")
+	
 	if message.chat.type == "private":
 		bot.reply_to(message, "Hi, bots are not alowed to message poeple that have not messaged them first ,that is why you have to start the chat with me to play the game\nDo not forget to run the /join command")
 
 		
 
 @bot.message_handler(commands=['new_game'])
-def new_game_resistance(message):
+def NewGame(message):
 	if message.chat.type == "private":
 		bot.reply_to(message, "You can't make a new game in a private chat")
-	elif message.chat.type != "private" and bot.game_state > 0:
+		return
+		
+	if message.chat.type != "private" and bot.game_state > 0:
 		bot.reply_to(message, "There is a game already made")
-	elif message.chat.type != "private" and bot.game_state ==0:
+		return
+	
+	if message.chat.type != "private" and bot.game_state ==0:
 		bot.reply_to(message, "To play resistance we need 5 to 10 people \nIf you want to play type /join in a private chat with me\nOnce everyone is ready have someone run the /start_game")
 		bot.game_room = message.chat.id 
 		bot.game_state = 1
@@ -43,18 +52,28 @@ def new_game_resistance(message):
 	
 	
 @bot.message_handler(commands=['join'])
-def join_game_resistance(message):
+def JoinGameResistance(message):
 	if message.chat.type != "private":
 		bot.reply_to(message, "Sorry you must run the join command in a private chat with me")
-	elif len(bot.players) == 10:
+		return
+	
+	if len(bot.players) == 10:
 		bot.reply_to(message, "The game already has the max number of players")
-	elif bot.game_state > 1:
+		return
+	
+	if bot.game_state > 1:
 		bot.reply_to(message, "The game has already started")
-	elif bot.game_state == 0:
+		return
+		
+	if bot.game_state <= 0:
 		bot.reply_to(message, "There is no game to join")
-	elif bot.game_state == 1 and message.from_user.id in bot.players:
+		return
+		
+	if bot.game_state == 1 and message.from_user.id in bot.players:
 		bot.reply_to(message, "You have already joined the game")		
-	elif bot.game_state == 1 and message.from_user.id not in bot.players:
+		return
+		
+	if bot.game_state == 1 and message.from_user.id not in bot.players:
 		bot.reply_to(message, "You have joined the game")
 		bot.send_message(bot.game_room, message.from_user.first_name +" has joined the game")
 		bot.players.append(message.from_user.id)
@@ -62,37 +81,60 @@ def join_game_resistance(message):
 		
 		
 @bot.message_handler(commands=['start_game'])
-def start_game_resistance(message):
-	if bot.game_state > 0: 
-		if bot.game_state > 1 and message.chat.id == bot.game_room:
-			bot.reply_to(message, "The game has already started")
-		if bot.game_state == 1 and message.chat.id == bot.game_room and len(bot.players) < 5:
-			bot.reply_to(message, "Not enough players have joined to start the game \nYou need 5 to 10 people to play, "+str(len(bot.players))+" have joined")
-		if bot.game_state == 1 and message.chat.id == bot.game_room and len(bot.players) >= 5 :
-			bot.reply_to(message, "Let the game begin")
-			bot.game_state = 2
-			assign_roles()
-		else:
-			bot.reply_to(message, "There's a time and place for everything, but not now")
-	else:
+def StartGameResistance(message):
+	if bot.game_state <= 0:
 		bot.reply_to(message, "There is no game to start")
+		return
+	
+	if bot.game_state > 1 and message.chat.id == bot.game_room:
+		bot.reply_to(message, "The game has already started")
+		return
 		
+	if bot.game_state == 1 and message.chat.id == bot.game_room and len(bot.players) < 5:
+		bot.reply_to(message, "Not enough players have joined to start the game \nYou need 5 to 10 people to play, "+str(len(bot.players))+" have joined")
+		return
 		
+	if bot.game_state == 1 and message.chat.id == bot.game_room and len(bot.players) >= 5 :
+		bot.reply_to(message, "Let the game begin")
+		bot.game_state = 2
+		AssignRoles()
+		return
 		
-def assign_roles():
-		bot.number_of_players = len(bot.players)
-		bot.number_of_spys = int(math.ceil(bot.number_of_players/3))
+	else:
+		bot.reply_to(message, "There's a time and place for everything, but not now")
+	
+
 		
-		random.shuffle(bot.players)
+def AssignRoles():
+	bot.number_of_players = len(bot.players)
+	bot.number_of_spys = int(math.ceil(bot.number_of_players/3))
+	
+	random.shuffle(bot.players)
+	
+	bot.send_message(bot.game_room, "There are will be "+str(bot.number_of_spys)+" Spys in this game" )
+	
+	for i in range(bot.number_of_spys):
+		bot.send_message(bot.players[i], "You are a Spy")
+		bot.spys.append(bot.players[i])
+	
+	index_shift = bot.number_of_players - bot.number_of_spys - 1
+	for i in range(bot.number_of_players - bot.number_of_spys):
+		bot.send_message(bot.players[i + index_shift], "You are Resistance")
+"""	
+	#GameHandler()
 		
-		bot.send_message(bot.game_room, "There are will be "+str(bot.number_of_spys)+" Spys in this game" )
-		bot.send_message(bot.players[0], "You are a Spy")
-		bot.send_message(bot.players[1], "You are a Spy")
-		bot.send_message(bot.players[2], "You are Resistance")
-		bot.send_message(bot.players[3], "You are Resistance")
-		bot.send_message(bot.players[4], "You are Resistance")
 		
 
-			
+def GameHandler():
+	bot.round_number = 0
+	while bot.points_resistance < 3 and bot.points_spys <3:
+		RoundHandler(bot.round_number)
+		bot.round_number += 1
+	
+	if bot.points_resistance == 3
+	if bot.points_spys == 3
+"""
+	
+	
 		
 bot.polling()
