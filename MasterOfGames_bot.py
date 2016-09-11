7fa116184d19567116e08b45f60c60724e647f11
Zachary R <zachary.mt.ranes@gmail.com>
2016-09-11 01:40:20 -0400

    starting message is edited with inline button presses

--- MasterOfGames_bot.py
+++ MasterOfGames_bot.py
@@ -26,6 +26,7 @@ def player_greatting(message):
 		
 	else:
 		bot.reply_to(message, "There's a time and place for everything, but not now")
+
 		
 
 @bot.message_handler(commands=['new_game'])
@@ -37,46 +38,55 @@ def new_game(message):
 	if message.chat.type == "group" and message.chat.id not in games:
 		markup = types.InlineKeyboardMarkup()
 		markup.row(types.InlineKeyboardButton(callback_data="resistance", text="Resistance"))
-		bot.reply_to(message, "Which game would you like to play?", reply_markup=markup)
+		bot.send_message(message.chat.id, "Which game would you like to play?", reply_markup=markup)
 		return
 		
 	else:
 		bot.reply_to(message, "There's a time and place for everything, but not now")
 		
-		
+	
+	
 @bot.callback_query_handler(func=lambda call: call.message.chat.id not in games and call.data == "resistance")
 def start_resistance(call):
 	markup = types.InlineKeyboardMarkup()
 	markup.row(types.InlineKeyboardButton(callback_data="join", text="Join"), types.InlineKeyboardButton(callback_data="start", text="Start Game"))
-	bot.send_message(call.message.chat.id, "You have selected the game resistance\nTo play resistance we need 5 to 10 people", reply_markup=markup)
+	bot.edit_message_text("You have selected the game resistance\nTo play resistance we need 5 to 10 people", message_id=call.message.message_id, chat_id=call.message.chat.id, reply_markup=markup)
 	new_game = Resistance()
 	games[call.message.chat.id] = new_game
+	
 	
 	
 @bot.callback_query_handler(lambda call: call.message.chat.id in games and call.data == "join")
 def player_join_game(call):
 	key = call.message.chat.id
 	player_id = call.from_user.id
+	
 	if player_id not in games[key].players: 
 		games[key].players.append(player_id)
 		bot.send_message(key, call.from_user.first_name +" has joined the game")
 
 	
+	
 @bot.callback_query_handler(lambda call: call.message.chat.id in games and call.data == "start")
 def start_game(call):
 	key = call.message.chat.id
 	game = games[key]
+	
 	if game.game_started != True:
-		#To-do make it so the bot update an old message about number of players joined not just sending an new message
 		if len(game.players) < game.MINPLAYERS:
-			bot.send_message(key, "Not enough players have joined to start the game \nYou need "+str(game.MINPLAYERS)+" to "+str(game.MAXPLAYERS)+" people to play, "+str(len(game.players))+" have joined")
-			return
-		
+			markup = types.InlineKeyboardMarkup()
+			markup.row(types.InlineKeyboardButton(callback_data="join", text="Join"), types.InlineKeyboardButton(callback_data="start", text="Start Game"))
+			try:
+				bot.edit_message_text("Not enough players have joined to start the game \nYou need "+str(game.MINPLAYERS)+" to "+str(game.MAXPLAYERS)+" people to play, "+str(len(game.players))+" player(s) has joined", message_id=call.message.message_id, chat_id=key, reply_markup=markup)
+			except:
+				return
+				
 		if len(game.players) >= game.MINPLAYERS:
-			bot.send_message(key, "Game start, Placeholder")
+			bot.send_message(key, "Let the game begin, Placeholder")
 			game_handler_one(key)
 			return
 
+			
 	
 def game_handler_one(key):
 	game = games[key]
@@ -93,6 +103,11 @@ def game_handler_one(key):
 		
 	except:
 		bot.send_message(key, "I can not talk to everyone \nEveryone please makesure you have a private chat with me open")
+
+
+		
+#def game_handler_two(key):
+
 
 		
 		
